9ac83e9730c606ec82c60564959d784f412cec2a
Bane Sullivan <banesullivan@gmail.com>
2025-03-15 18:04:13 -0700

    Update build message

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e -u -x
-echo "Cloning VTK version ${VTK_VERSION:-v9.3.1}..."
+
+echo "Cloning VTK version ${VTK_VERSION:-v9.3.1} for ${VTK_VARIANT:-osmesa} variant"
+
 git clone https://gitlab.kitware.com/vtk/vtk.git --depth 1 --branch "${VTK_VERSION:-v9.3.1}" vtk
 mkdir -p vtk/build
 cp cmake/*.cmake vtk/build/
